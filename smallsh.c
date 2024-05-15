96a330322d4bb34a6c4280e367848be828fac9e5
Judah Jackson <jacksjud@flip3.engr.oregonstate.edu>
2024-05-15 14:41:10 -0700

    exit functionality implemented. Also, comments of all variety work now.

--- smallsh.c
+++ smallsh.c
@@ -45,21 +45,25 @@ Dev Notes:
 /* Struct(s) */
 
 typedef struct Command {
-    char command[MAX_ARGS];
+    char * command[MAX_ARGS + 1];
     short int skip;
     short int builtin;
     short int background;
     int argCount;
-
 } Command;
 
 /* Function Prototypes*/
 
 Command * parseCommand();
+char* replaceToken(char *, char *);
 void execCommand(Command *);
+void execExit();
+void execCd(Command *);
+void execStatus(Command *);
+void execOther(Command *);
 void freeCommand(Command *);
 char * checkExpansion(char *, char *);
-char* replaceToken(char *, char *);
+
 void test_replaceToken();
 
 
@@ -82,7 +86,7 @@ Params: N/A
 ---------------------------------------- */
 int main(){
 
-    test_replaceToken();
+    //test_replaceToken();
 
     while(1){
         // Parse command 
@@ -92,6 +96,7 @@ int main(){
             continue;
         }
         execCommand(command);
+        freeCommand(command);
     }
 
     
@@ -113,46 +118,80 @@ Params: N/A
 Command * parseCommand(){
     // Struct that represents a command 
     Command * command = (Command *)malloc(sizeof(Command));
+    if (command == NULL) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        exit(EXIT_FAILURE);
+    }
     command->argCount = 0;                  // Initially no command arguments
+    command->skip = 0;
+    command->builtin = 0;
+    command->background = 0;
 
     printf(": ");                           // Prompt user interaction, make sure it's output
     fflush(stdout);
-
+    
     // Represents command given to shell in string form (max size)
     char commandStr[MAX_CHARS];
     fgets(commandStr, MAX_CHARS, stdin);        // Gets command and saves in string variable
 
     char * token = strtok(commandStr , " ");    // Gets token (argument) based on a space (' ') as the delimiter
-    
+
+
     // Checks if comment or blank - only applicable to first argument (command)
-    if(token[0] == "#" || token[0] == "\n"){
+    if( strcmp(token, "\n") == 0 || strncmp(token , "#", 1) == 0){
         command->skip = 1;
         return command;
     }
 
     // Continue until we reach the end (cannot be NULL, or ending character '\0')
-    while(token != NULL || strcmp(token, "\0" != 0)){
+    while(token != NULL && strcmp(token, "\0") != 0){
         // Check if token has variable that needs to be replaced
         char * position = strstr(token, "$$");
         if(position != NULL){
             replaceToken(token, position);
         }
+
+        if(command->argCount >= MAX_ARGS){
+            fprintf(stderr, "MAX ARGS REACHED == \n");
+            exit(EXIT_FAILURE);
+        }
+
+        // Allocate memory for command arg
+        command->command[command->argCount] = (char *)malloc(MAX_CHARS);
+        if (command->command[command->argCount] == NULL) {
+            fprintf(stderr, "Memory allocation failed.\n");
+            exit(EXIT_FAILURE);
+        }
+
+
         // Copies token to command argument array
-        strncpy(command->command[command->argCount], token, strlen(token));
+        strncpy(command->command[command->argCount], token, MAX_ARGS - 1);
+        //command->command[command->argCount][MAX_CHARS - 1] = '\0'; // Ensure null-termination
         command->argCount++;                    // Increment arg count
         token = strtok(NULL, " ");              // Update token to point to next
     }
+
+    command->command[command->argCount] = (char *)malloc(2);
+    strncpy(command->command[command->argCount], "\0", 2);
+    command->argCount++;
+
     
     // Checks if meant to executed in background
-    if(command->command[command->argCount] == "&"){
+    if(strcmp(command->command[command->argCount - 1], "&") == 0 || strcmp(command->command[command->argCount - 1], "&\n") == 0){
+        printf("background! == %s", command->command[command->argCount - 1]);
         command->background = 1;
     }
 
-    // Checks if built in command
-    if(command->command[0] == "exit" || command->command[0] == "cd" || command->command[0] == "status"){
+    // Checks if built in command, give appropriate code
+    if(strcmp(strtok(command->command[0] , "\n"), "exit") == 0){
         command->builtin = 1;
     }
-
+    else if(strcmp(strtok(command->command[0], "\n"), "cd") == 0) {
+        command->builtin = 2;
+    }
+    else if(strcmp(strtok(command->command[0], "\n"), "status") == 0) {
+        command->builtin = 3;
+    }
     return command;
 }
 
@@ -166,9 +205,134 @@ Params:
 
 void execCommand(Command * command){
 
+    // If command meant to be ran in background
+    if(command->background){
+        /* 
+        We want this to execute, but we don't want
+        the parent to wait, either way it's going to fork
+        */
+    }
+    
+    pid_t spawnpid = -5;
+	int childStatus;
+    int childPid;
+    // If fork is successful, the value of spawnpid will be 0 in the child, the child's pid in the parent
+    spawnpid = fork();
+    switch (spawnpid){
+        case -1:
+            perror("fork() failed!");
+            exit(1);
+        case 0:
+            // spawnpid is 0 in the child
+            printf("I am the child. My pid  = %d\n", getpid());
+            switch(command->builtin){
+                case 1:
+                    freeCommand(command);
+                    execExit();
+                    break;
+                case 2:
+                    execCd(command);
+                    break;
+                case 3:
+                    execStatus(command);
+                    break;
+                default:
+                    execOther(command);
+                    break;
+            }
+            break;
+        default:
+            // spawnpid is the pid of the child
+            printf("I am the parent. My pid  = %d\n", getpid());
+            // If command meant to be ran in background
+            if(command->background){
+                /* 
+                We want this to execute, but we don't want
+                the parent to wait, either way it's going to fork
+                */
+                printf("Command meant for background - parent returning: %d\n", getpid());
+                return;
+            }
+            childPid = waitpid(spawnpid , &childStatus, 0);
+            printf("Parent's waiting is done as the child with pid %d exited\n", childPid);
+            break;
+    }
+}
 
 
-    freeCommand(command);
+
+/* ----------------------------------------
+    Function: execExit
+///////////////////////////////////////////
+Desc: Kills any other processes or jobs that
+the shell has started before it terminates itself.
+
+Params: N/A
+---------------------------------------- */
+void execExit(){
+    kill(0, SIGTERM);       // Send signal to all processes 
+    exit(EXIT_SUCCESS);     // Exit shell
+}
+
+
+/* ----------------------------------------
+    Function: execCd
+///////////////////////////////////////////
+Desc: Changes the working directory of smallsh.
+If no arguments are given, it changes to the 
+directory specified in the HOME environment variable 
+
+Params:
+command: Command *
+---------------------------------------- */
+void execCd(Command * command){
+    // Check if cd has no other arguments, in which case cd HOME
+    if(strcmp(command->command[1], "\n") == 0 || strcmp(command->command[1], "\0") == 0){ 
+        chdir(getenv("HOME"));
+        system("ls");
+        return;
+    }
+    if(chdir(strtok(command->command[1], "\n")) != 0){
+        perror("Error with chdir");
+    }
+    system("ls");
+}
+
+
+/* ----------------------------------------
+    Function: execStatus
+///////////////////////////////////////////
+Desc: Prints out either the exit status or the 
+terminating signal of the last foreground process 
+ran by the shell. If ran before any foreground 
+command is run, then it simply returns the exit 
+status 0.
+
+Params: 
+command: Command *
+---------------------------------------- */
+void execStatus(Command * command){
+
+}
+
+
+/* ----------------------------------------
+    Function: execOther
+///////////////////////////////////////////
+Desc: 
+
+Params:
+---------------------------------------- */
+void execOther(Command * command){
+
+}
+
+
+void freeCommand(Command * command){
+    for(int i = 0; i < command->argCount ; i++){
+        free(command->command[i]);
+    }
+    free(command);
 }
 
 /* ----------------------------------------
@@ -204,7 +368,7 @@ char* replaceToken(char *token, char *substring){
     strncpy(substring, pidStr, pidLen);     // Copy the process ID string into the token, in the new space we just created.
 
     // Check again to see if there are anymore, if so, recurse.
-    char *newSubstring = strstr(token, "$$");
+    char * newSubstring = strstr(token, "$$");
     if (newSubstring != NULL) {
         replaceToken(token, newSubstring);
     }
@@ -217,8 +381,8 @@ void test_replaceToken() {
     char *substring = strstr(token, "$$");
     if (substring != NULL) {
         printf("Original token: %s\n", token);
-        char *new_token = replaceToken(token, substring);
-        printf("Modified token: %s\n", new_token);
+        replaceToken(token, substring);
+        printf("Modified token: %s\n", token);
     } else {
         printf("Substring not found\n");
     }
